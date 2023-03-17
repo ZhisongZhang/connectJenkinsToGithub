83fb7e0899bab0eb4f93f4573c8bc148e3d5459c
Zhisong Zhang (DEECA) <zhisong.zhang@delwp.vic.gov.au>
2023-03-17 13:28:20 +1100

    testing 1

--- algorithm.sh
+++ algorithm.sh
@@ -3,4 +3,5 @@
 # echo "3.14159" | tee -a report.txt
 
 # UNCOMMENT THE FOLLOWING LINE TO GET A MORE PRECISE CALCULATION OF PI. ALSO REMOVE THE PREVIOUS ECHO STATEMENTS.
-echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+# echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
+echo "scale=1000; 4*a(1)" | tee report.txt
